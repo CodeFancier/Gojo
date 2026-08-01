ab762d62413ffc6aa6387760078d511283a215df
Fancy Jiang <28106693+CodeFancier@users.noreply.github.com>
2026-08-01 21:28:45 +0800

    fix: remove untracked view dependencies

--- Sources/Gojo/Views/ShelfView.swift
+++ Sources/Gojo/Views/ShelfView.swift
@@ -5,16 +5,5 @@ struct ShelfView: View {
     var body: some View {
         CodingSpaceCarousel()
         .background(DomainBackground())
-        .overlay(alignment: .topLeading) {
-            BrandWordmark(height: 26)
-                .padding(.leading, 84)
-                .padding(.top, 14)
-        }
-        .overlay(alignment: .bottomTrailing) {
-            AuthorCredit()
-                .padding(.trailing, 16)
-                .padding(.bottom, 12)
-        }
     }
-
 }
